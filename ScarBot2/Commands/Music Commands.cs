13d7e745ea2459050734f81e7b271d21a27b8f46
William Ward <williamw1979@netscape.net>
2025-04-06 05:39:07 -0400

    Remove old files

---
+++ ScarBot2/Commands/Music Commands.cs
@@ -0,0 +1,73 @@
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+
+public class MusicCommands
+{
+    private readonly MusicHandler _musicHandler;
+
+    public MusicCommands(MusicHandler musicHandler)
+    {
+        _musicHandler = musicHandler;
+    }
+
+    [SlashCommand("join", "Join a voice channel")]
+    public async Task JoinVoiceChannelAsync(SocketVoiceChannel voiceChannel)
+    {
+        await _musicHandler.JoinVoiceChannel(voiceChannel);
+    }
+
+    [SlashCommand("leave", "Leave the current voice channel")]
+    public async Task LeaveVoiceChannelAsync(SocketVoiceChannel voiceChannel)
+    {
+        await _musicHandler.LeaveVoiceChannel(voiceChannel);
+    }
+
+    [SlashCommand("play", "Play a song from YouTube")]
+    public async Task PlaySongAsync(string songUrl)
+    {
+        ulong serverId = Context.Guild.Id;
+        await _musicHandler.PlaySong(serverId, songUrl);
+    }
+
+    [SlashCommand("stop", "Stop the current song")]
+    public async Task StopSongAsync()
+    {
+        ulong serverId = Context.Guild.Id;
+        await _musicHandler.StopSong(serverId);
+    }
+
+    [SlashCommand("pause", "Pause the current song")]
+    public async Task PauseSongAsync()
+    {
+        ulong serverId = Context.Guild.Id;
+        await _musicHandler.PauseSong(serverId);
+    }
+
+    [SlashCommand("resume", "Resume the paused song")]
+    public async Task ResumeSongAsync()
+    {
+        ulong serverId = Context.Guild.Id;
+        await _musicHandler.ResumeSong(serverId);
+    }
+
+    [SlashCommand("skip", "Skip to the next song")]
+    public async Task SkipSongAsync()
+    {
+        ulong serverId = Context.Guild.Id;
+        await _musicHandler.SkipSong(serverId);
+    }
+
+    [SlashCommand("repeat", "Set the repeat mode")]
+    public async Task SetRepeatAsync(bool repeat)
+    {
+        ulong serverId = Context.Guild.Id;
+        await _musicHandler.SetRepeat(serverId, repeat);
+    }
+
+    [SlashCommand("247", "Set the 247 mode")]
+    public async Task Set247Async(bool is247)
+    {
+        ulong serverId = Context.Guild.Id;
+        await _musicHandler.Set247(serverId, is247);
+    }
+}
---
+++ ScarBot2/Music/Music Handler.cs
@@ -0,0 +1,112 @@
+﻿using Discord.WebSocket;
+using LoggingLib;
+using MYSQL;
+
+public class MusicHandler
+{
+    private readonly IAudioService _audioService;
+    private readonly MySQL _mysql;
+    private readonly Dictionary<ulong, MusicQueue> _queues = new Dictionary<ulong, MusicQueue>(); // Queue per server
+    private readonly Dictionary<ulong, MusicSettings> _musicSettings = new Dictionary<ulong, MusicSettings>(); // Settings per server
+
+    public MusicHandler(IAudioService audioService, MySQL mysql)
+    {
+        _audioService = audioService;
+        _mysql = mysql;
+    }
+
+    public async Task JoinVoiceChannel(SocketVoiceChannel voiceChannel)
+    {
+        await _audioService.JoinChannel(voiceChannel);
+        ScarBot.Log($"Joined channel {voiceChannel.Name}.");
+    }
+
+    public async Task LeaveVoiceChannel(SocketVoiceChannel voiceChannel)
+    {
+        await _audioService.LeaveChannel(voiceChannel);
+        ScarBot.Log($"Left channel {voiceChannel.Name}.");
+    }
+
+    public async Task PlaySong(ulong serverId, string songUrl)
+    {
+        var queue = _queues.ContainsKey(serverId) ? _queues[serverId] : new MusicQueue();
+        queue.AddSong(songUrl);
+        _queues[serverId] = queue;
+
+        var song = await YouTubeExplodeClient.GetSongAsync(songUrl);
+        await _audioService.PlaySong(song);
+
+        ScarBot.Log($"Playing song: {song.Title}");
+    }
+
+    public async Task PauseSong(ulong serverId)
+    {
+        await _audioService.Pause();
+        ScarBot.Log($"Paused song for server {serverId}");
+    }
+
+    public async Task StopSong(ulong serverId)
+    {
+        await _audioService.Stop();
+        ScarBot.Log($"Stopped song for server {serverId}");
+    }
+
+    public async Task ResumeSong(ulong serverId)
+    {
+        await _audioService.Resume();
+        ScarBot.Log($"Resumed song for server {serverId}");
+    }
+
+    public async Task SkipSong(ulong serverId)
+    {
+        var queue = _queues.ContainsKey(serverId) ? _queues[serverId] : new MusicQueue();
+        queue.SkipSong();
+        _queues[serverId] = queue;
+
+        var song = await YouTubeExplodeClient.GetSongAsync(queue.Songs[queue.CurrentSongIndex]);
+        await _audioService.PlaySong(song);
+
+        ScarBot.Log($"Skipped to next song for server {serverId}");
+    }
+
+    public async Task SetRepeat(ulong serverId, bool repeat)
+    {
+        if (!_musicSettings.ContainsKey(serverId))
+            _musicSettings[serverId] = new MusicSettings();
+
+        _musicSettings[serverId].Repeat = repeat;
+        ScarBot.Log($"Set repeat for server {serverId} to {repeat}");
+    }
+
+    public async Task Set247(ulong serverId, bool is247)
+    {
+        if (!_musicSettings.ContainsKey(serverId))
+            _musicSettings[serverId] = new MusicSettings();
+
+        _musicSettings[serverId].Is247 = is247;
+        ScarBot.Log($"Set 247 for server {serverId} to {is247}");
+    }
+}
+
+public class MusicQueue
+{
+    public List<string> Songs { get; private set; } = new List<string>();
+    public int CurrentSongIndex { get; set; } = 0;
+
+    public void AddSong(string songUrl)
+    {
+        Songs.Add(songUrl);
+    }
+
+    public void SkipSong()
+    {
+        if (CurrentSongIndex < Songs.Count - 1)
+            CurrentSongIndex++;
+    }
+}
+
+public class MusicSettings
+{
+    public bool Repeat { get; set; } = false;
+    public bool Is247 { get; set; } = false;
+}
